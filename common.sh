f4527cf4c5975effe10a0c3b0f33621c3fbd1bfe
Yuriy Belikov <yuriybelikov1@gmail.com>
2024-02-23 00:09:56 +0200

    Add scripts to create directories and setup overlayfs with metacopy feature on/off

---
+++ common.sh
@@ -0,0 +1,26 @@
+create_overlay_fs_working_dirs() 
+{
+    if [ ! -d $1 ]; then
+        mkdir -p "$1"
+    fi
+
+    if [ ! -d $2 ]; then
+        mkdir -p "$2"
+    fi
+
+    if [ ! -d $3 ]; then
+        mkdir -p "$3"
+    fi  
+
+    if [ ! -d $4 ]; then
+        mkdir -p "$4"
+    fi
+}
+
+mount_overlay_fs()  
+{
+    create_overlay_fs_working_dirs $1 $2 $3 $4
+    echo "lower: $1, upper: $2, work: $3, merge: $4, metacopy: $5"
+    sudo mount -t overlay overlay -olowerdir=$1,upperdir=$2,workdir=$3,metacopy=$5 $4
+}
+
---
+++ overlay_fs_mount_metadata_copy_on.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ./common.sh
+OVERLAY_FS_MOUNT_POINT="overlay_fs_test_metacopy"
+UPPER_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/write_dir
+LOWER_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/readonly_dir
+MERGE_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/merge_dir
+WORK_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/work_dir
+
+mount_overlay_fs $LOWER_DIR $UPPER_DIR $WORK_DIR $MERGE_DIR on
