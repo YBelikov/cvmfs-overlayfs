8e8b066c850fbe682aed735b9c8d3f828b462ca9
Yuriy Belikov <yuriybelikov1@gmail.com>
2024-03-05 11:49:26 +0200

    Remove metacopy folder

--- metacopy/overlay_fs_mount_metadata_copy_off.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./common.sh
-OVERLAY_FS_MOUNT_POINT="overlayfs_metacopy_off"
-UPPER_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/write_dir
-LOWER_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/readonly_dir
-MERGE_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/merge_dir
-WORK_DIR=/home/$USER/$OVERLAY_FS_MOUNT_POINT/work_dir
-
-mount_overlay_fs $LOWER_DIR $UPPER_DIR $WORK_DIR $MERGE_DIR off
